56724f0df51aa5f8b7a3f34168089d93acf6f066
mcakavithas <mcakavithas@gmail.com>
2014-05-31 13:18:57 +0530

    updated database file

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,32 +1,30 @@
 class Deposit < ActiveRecord::Base
  attr_accessible :amount, :n, :periods, :roi, :total, :dob, :name, :age
-
-before_save :calculate_value
-#Calculate the Savings Deposit
-def calculate_value
-self.total = amount * (1 + (roi / 12)) ** (12 * periods)
-end
-
  before_save :calculate_age
-
  def calculate_age
  	self.age = ((DateTime.now - dob) / 365.25).to_i
  end
 
+
 before_save :calculate_interest
-
 def calculate_interest
-if age >= 1 && age <=57
-self.roi
-end
-if age >= 58 && age < 75
-self.roi += 0.005
-end
-if age >= 75
-self.roi += 0.01
-end
+  if self.age <=57
+    self.roi
+  elsif self.age >= 58 && self.age <= 75 && self.periods = 5 
+    self.roi += 0.0025
+  elsif self.age >= 75 && self.periods = 5
+    self.roi += 0.0025
+  else
+    self.roi
+  end
+ end
 
-end
 
+before_save :calculate_value
+#Calculate the Savings Deposit
+#attr_accessor :amount
 
+def calculate_value
+  self.total = amount * (1 + (roi / 12)) ** (12 * periods)
+end	
 end
--- db/migrate/20140516095908_remove_type_from_deposits.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTypeFromDeposits < ActiveRecord::Migration
-  def up
-    remove_column :deposits, :type
-  end
-
-  def down
-    add_column :deposits, :type, :string
-  end
-end
